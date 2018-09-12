72aa6280521b3c787f173c333782dcd68bc4a28f
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2018-09-12 18:47:56 +0900

    Added features of changin line width and an indicator for how large of line width

--- README.md
+++ README.md
@@ -2,22 +2,26 @@
 
 以下のブログ記事で利用するソースコードをこのレポジトリに用意
 
-https://tsuyopon.xyz/2018/09/14/how-to-create-drawing-app-part2/
+https://tsuyopon.xyz/2018/09/14/how-to-create-drawing-app-part3/
 
 ## 動作確認
 
 下記ページよりこのレポジトリで作成した「お絵かきアプリ」を利用することが出来ます。
 
-https://tsuyopon-xyz.github.io/drawing_app_part2/
+https://tsuyopon-xyz.github.io/drawing_app_part3/
 
 ## 前回からの変更点
 
-- jsディレクトリを作って、main.jsをjsディレクトリの中に移動
-- cssディレクトリ作成
-- 色変更用のカラーパレットの実装に [`colorjoe`](https://github.com/bebraw/colorjoe) というライブラリを追加
-  - jsディレクトリに[colorjoe.min.js](https://raw.githubusercontent.com/bebraw/colorjoe/master/dist/colorjoe.min.js)を追加
-  - cssディレクトリに[colorjoe.css](https://raw.githubusercontent.com/bebraw/colorjoe/master/css/colorjoe.css)を追加ｖ
+- HTMLに線の太さを変更できるinput[type=range]要素を追加
+- canvas要素を1つ追加
+  - マウスの位置を中心として、線の太さを「○」で表現するためのcanvas
+- 前回から用意している線の描画用canvasのstyleに「position: absolute;」を追加
+  - 2つのcanvasを重ねるため
+- 2つのcanvasを小要素に持つ`<span id="layerd-canvas-area">`を追加
+  - 前回までは線を描画するためのcanvasにマウスイベントを定義していたが、今回の変更で`<span id="layerd-canvas-area">`に対してマウスイベントを定義するように変更した。
+    - 2つのcanvasを同時に描画するため
+- 文字の太さの設定・更新を行うためのinitConfigOfLineWidth関数を用意
 
 ## 前回のレポジトリ
 
-https://github.com/tsuyopon-xyz/drawing_app_part1
+https://github.com/tsuyopon-xyz/drawing_app_part2
--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@
 window.addEventListener('load', () => {
   const canvas = document.querySelector('#draw-area');
   const context = canvas.getContext('2d');
+
+  // 現在のマウスの位置を中心に、現在選択している線の太さを「○」で表現するために使用するcanvas
+  const canvasForWidthIndicator = document.querySelector('#line-width-indicator');
+  const contextForWidthIndicator = canvasForWidthIndicator.getContext('2d');
+
   const lastPosition = { x: null, y: null };
   let isDrag = false;
   let currentColor = '#000000';
@@ -35,6 +40,31 @@ window.addEventListener('load', () => {
     lastPosition.y = y;
   }
 
+  // <canvas　id="line-width-indicator"> 上で現在のマウスの位置を中心に
+  // 線の太さを表現するための「○」を描画する。
+  function showLineWidthIndicator(x, y) {
+    contextForWidthIndicator.lineCap = 'round';
+    contextForWidthIndicator.lineJoin = 'round';
+    contextForWidthIndicator.strokeStyle = currentColor;
+
+    // 「○」の線の太さは細くて良いので1で固定
+    contextForWidthIndicator.lineWidth = 1;
+
+    // 過去に描画「○」を削除する。過去の「○」を削除しなかった場合は
+    // 過去の「○」が残り続けてします。(以下の画像URLを参照)
+    // https://tsuyopon.xyz/wp-content/uploads/2018/09/line-width-indicator-with-bug.gif
+    contextForWidthIndicator.clearRect(0, 0, canvasForWidthIndicator.width, canvasForWidthIndicator.height);
+
+    contextForWidthIndicator.beginPath();
+
+    // x, y座標を中心とした円(「○」)を描画する。
+    // 第3引数の「currentLineWidth / 2」で、実際に描画する線の太さと同じ大きさになる。
+    // ドキュメント: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+    contextForWidthIndicator.arc(x, y, currentLineWidth / 2, 0, 2 * Math.PI);
+
+    contextForWidthIndicator.stroke();
+  }
+
   function clear() {
     context.clearRect(0, 0, canvas.width, canvas.height);
   }
@@ -60,11 +90,37 @@ window.addEventListener('load', () => {
       currentColor = '#FFFFFF';
     });
 
-    canvas.addEventListener('mousedown', dragStart);
-    canvas.addEventListener('mouseup', dragEnd);
-    canvas.addEventListener('mouseout', dragEnd);
-    canvas.addEventListener('mousemove', (event) => {
+    // layeredCanvasAreaは2つのcanvas要素を保持している。2つのcanvasはそれぞれ以下の役割を持つ
+    //
+    // 1. 絵を書くためのcanvas
+    // 2. 現在のマウスの位置を中心として、太さを「○」の形で表現するためのcanvas
+    //
+    // 1と2の機能を1つのキャンパスで共存することは出来ない。
+    // 共存できない理由は以下の通り。
+    //
+    // - 1の機能は過去に描画してきた線の保持し続ける
+    // - 2の機能は前回描画したものを削除する必要がある。削除しなかった場合は、過去の「○」が残り続けてしまう。(以下の画像URLを参照)
+    //   - https://tsuyopon.xyz/wp-content/uploads/2018/09/line-width-indicator-with-bug.gif
+    //
+    // 上記2つの理由より
+    // - 1のときはcontext.clearRectを使うことが出来ず
+    // - 2のときはcontextForWidthIndicator.clearRectを使う必要がある
+    const layeredCanvasArea = document.querySelector('#layerd-canvas-area');
+
+    // 元々はcanvas.addEventListenerとしていたが、
+    // 2つのcanvasを重ねて使うようになったため、親要素である <span id="layerd-canvas-area">に対して
+    // イベント処理を定義するようにした。
+    layeredCanvasArea.addEventListener('mousedown', dragStart);
+    layeredCanvasArea.addEventListener('mouseup', dragEnd);
+    layeredCanvasArea.addEventListener('mouseout', dragEnd);
+    layeredCanvasArea.addEventListener('mousemove', event => {
+      // 2つのcanvasに対する描画処理を行う
+
+      // 実際に線を引くcanvasに描画を行う。(ドラッグ中のみ線の描画を行う)
       draw(event.layerX, event.layerY);
+
+      // 現在のマウスの位置を中心として、線の太さを「○」で表現するためのcanvasに描画を行う
+      showLineWidthIndicator(event.layerX, event.layerY);
     });
   }
 
@@ -79,6 +135,9 @@ window.addEventListener('load', () => {
   function initConfigOfLineWidth() {
     const textForCurrentSize = document.querySelector('#line-width');
     const rangeSelector = document.querySelector('#range-selector');
+
+    // 線の太さを記憶している変数の値を更新する
+    currentLineWidth = rangeSelector.value;
 
     // "input"イベントをセットすることでスライド中の値も取得できるようになる。
     // ドキュメント: https://developer.mozilla.org/ja/docs/Web/HTML/Element/Input/range
